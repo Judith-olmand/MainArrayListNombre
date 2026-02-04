87c193380449dff6e4638224d06fe279386b446c
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-04 18:26:57 +0100

    Acabado

--- src/MainArrayListNombre.java
+++ src/MainArrayListNombre.java
@@ -1,13 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class MainArrayListNombre {
+    public static void main(String[] args) {
+        List<String> nombres = new ArrayList<>();
+
+        nombres.add("Pedro");
+        nombres.add("Maria");
+        nombres.add("Rodrigo");
+        nombres.add("Antonio");
+
+        for(String nombre : nombres){
+            System.out.println(nombre);
+        }
+
+        System.out.println("***********************");
+        System.out.println("Elimino a Rodrigo y añado en la posición 0 a Ismael");
+        nombres.remove("Rodrigo");
+        nombres.add(0,"Ismael");
+
+
+        for(String nombre : nombres){
+            System.out.println(nombre);
+        }
+
+        System.out.println("***********************");
+        System.out.println("El nombre en la posición 2 es " + nombres.get(2));
+        System.out.println();
+
+        System.out.println("La lista tiene un total de " + nombres.size() + " elementos.");
+
+        nombres.clear();
+        System.out.println("Después de CLEAR la lista tiene " + nombres.size() + " elementos.");
+
     }
 }
